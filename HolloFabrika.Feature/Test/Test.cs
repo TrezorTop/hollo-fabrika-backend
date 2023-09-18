bdad8b7ae007a6dbdc6477f82dafbbe4c1548525
trezortop92@gmail.com <trezortop92@gmail.com>
2023-09-19 00:41:01 +0500

    workflow test

--- HolloFabrika.Feature/Test/Test.cs
+++ HolloFabrika.Feature/Test/Test.cs
@@ -6,10 +6,10 @@ public class Test
 {
     public static Result<TestModel> GetTest()
     {
-        var model = new TestModel(1, "Message");
+        var model = new TestModel(1, "Message 1233");
 
         if (Random.Shared.Next(1, 3) > 1)
-            return new Error("Error")
+            return new Error("Error 321")
                 .WithMetadata("item", model);
 
         return model;
